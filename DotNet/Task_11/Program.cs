7953a1c2d65be857615b09a7b90fc3711d2e8c3e
Alexey <a.v.alyokhin@mail.ru>
2022-06-27 15:39:44 +0500

    dobavil Task_11,Task_12 zadachi na seminare

---
+++ DotNet/Task_11/Program.cs
@@ -0,0 +1,27 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int rndNum = new Random().Next(101, 1000);
+Console.WriteLine($"Случайное число из отрезка 100 - 999 => {rndNum}");
+
+// int MaxDigit(int num)
+// {
+//     int firstDigit = num / 100;
+//     int lastDigit = num % 10;
+//     //if(firstDigit == lastDigit) return 0;
+//     //return firstDigit > lastDigit ? firstDigit : lastDigit;
+// }
+
+int firstDigit = rndNum / 100;
+int lastDigit = rndNum % 10;
+
+//int maxDigit = MaxDigit(rndNum);
+//string result = maxDigit > 0 ? maxDigit.ToString() : "числа равны";
+//Console.WriteLine($"Наибольшая цифра числа {firstDigit} => {lastDigit}");
+
+Console.WriteLine($"{firstDigit}{lastDigit}");
+Console.WriteLine(firstDigit*10 + lastDigit);
---
+++ DotNet/Task_S2_GW/Program.cs
@@ -0,0 +1,29 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли второе число
+// кратным первому. Если число 2 не кратно числу 1, то
+// программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+// int rndNum = new Random().Next(10, 100);
+// Console.WriteLine($"Случайное число из отрезка 10 - 99 => {rndNum}");
+
+// int MaxDigit(int num)
+// {
+//     int firstDigit = num / 10;
+//     int secondDigit = num % 10;
+//     if(firstDigit == secondDigit) return 0;
+//     return firstDigit > secondDigit ? firstDigit : secondDigit;
+// }
+
+// int maxDigit = MaxDigit(rndNum);
+// string result = maxDigit > 0 ? maxDigit.ToString() : "числа равны";
+// Console.WriteLine($"Наибольшая цифра числа {rndNum} => {result}");
