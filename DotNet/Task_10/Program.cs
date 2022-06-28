6795da3fcbd70542cc530b7d4a9f3b8427a9baab
Alexey <a.v.alyokhin@mail.ru>
2022-06-28 13:03:51 +0500

    Dobavil reshenie zadachi 10 - DZ#10 cherez metod

--- DotNet/Task_10/Program.cs
+++ DotNet/Task_10/Program.cs
@@ -3,3 +3,13 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+var randomGod = new Random();
+int rndNum = randomGod.Next(100, 1000);
+
+string num2identifier (int in_number)
+{
+    return ($"вторая цифра числа {in_number} -> {in_number % 100 / 10}");
+}
+
+Console.Write(num2identifier(rndNum));
