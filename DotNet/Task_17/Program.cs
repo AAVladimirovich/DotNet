a1680908db879763ba0cde46d434d750511f29d2
Alexey <a.v.alyokhin@mail.ru>
2022-07-01 12:41:28 +0500

    dobavil swtich case cherez 2 usloviya v Task_17

--- DotNet/Task_17/Program.cs
+++ DotNet/Task_17/Program.cs
@@ -17,17 +17,17 @@ int Y = Convert.ToInt32(Console.ReadLine());
 // else if (X > 0 && Y < 0) Console.WriteLine("chetvertaya chetvert'");
 // else Console.WriteLine("vvedeni nekorrektnie coordinati");
 
-string Voo(int in_X, int in_Y)
-{
-    switch ((in_X, in_Y))
-    {
-        case ( > 0, > 0): return "pervaya chetvert"; break;
-        case ( < 0, > 0): return "vtoraya chetvert"; break;
-        case ( < 0, < 0): return "tretya chetvert"; break;
-        case ( > 0, < 0): return "chetvertaya chetvert"; break;
-        default: return "vvedeni nekorrektnie coordinati"; break;
-    }
-}
+// string Voo(int in_X, int in_Y)
+// {
+//     switch ((in_X, in_Y))
+//     {
+//         case ( > 0, > 0): return "pervaya chetvert"; break;
+//         case ( < 0, > 0): return "vtoraya chetvert"; break;
+//         case ( < 0, < 0): return "tretya chetvert"; break;
+//         case ( > 0, < 0): return "chetvertaya chetvert"; break;
+//         default: return "vvedeni nekorrektnie coordinati"; break;
+//     }
+// }
 
 string Vooter(int in_X, int in_Y)
 {
