0c3d4c12ed6dfa1ee368b736b7deb93fd3679e13
Alexey <a.v.alyokhin@mail.ru>
2022-07-02 12:40:08 +0500

    dobavil Lection_03 foldet = DZ from lection urok 3 - vivesti massiv ot maksimuma

---
+++ DotNet/Lection_03/Program.cs
@@ -0,0 +1,34 @@
+﻿// программа для упорядочивания массива от максимального числа
+void PrintArray(int[] in_array)
+{
+    for (int i = 0; i < in_array.Length; i++)
+    {
+        Console.Write($"{in_array[i]},");
+    }
+    Console.WriteLine();
+}
+
+void SelectionMaxSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition])
+            {
+                maxPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+        PrintArray(array);
+    }
+}
+
+int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+SelectionMaxSort(arr);
+PrintArray(arr);
+
