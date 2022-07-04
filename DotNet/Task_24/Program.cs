2d36b27ef0c8b37405bc9014702de11138ed9034
Alexey <a.v.alyokhin@mail.ru>
2022-07-04 14:38:27 +0500

    dobavil Task_24 - rabota po seminaru

---
+++ DotNet/Task_24/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine(@"Задача 24: Напишите программу, которая
+принимает на вход число (А) и выдаёт сумму чисел
+от 1 до А.
+7 -> 28
+4 -> 10
+8 -> 36
+");
+
+int Sum(int in_numer)
+{
+
+    int valueForReturn = 0;
+
+    for (int i = 1; i <= in_numer; i++)
+    {
+        valueForReturn += i; // приравниваем к valueForReturn + i на каждом цикле
+    }
+
+    return valueForReturn;
+}
+
+Console.Write("Введите число А = ");
+int num = int.Parse(Console.ReadLine());
+
+int sum = Sum(num);
+Console.WriteLine($"Сумма всех чисел от 1 до {num} = {sum}");
