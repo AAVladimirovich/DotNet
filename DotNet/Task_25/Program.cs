462141148af44097ece3e9091c4c4b62521f0abe
Alexey <a.v.alyokhin@mail.ru>
2022-07-05 12:51:12 +0500

    dobavil Task_25 folder - dlya DZ#25 - proveril rabotaet

---
+++ DotNet/Task_25/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 25: Напишите цикл, который принимает
+// на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Stepen(int in_chislo, int in_stepen)
+{
+    int valueForReturn = in_chislo;
+    
+    checked
+    {
+        for (int i = 1; i < in_stepen; i++)
+        {
+            valueForReturn *= in_chislo;
+        }
+    }
+    
+    return valueForReturn;
+
+}
+
+Console.WriteLine(@"Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+");
+
+Console.WriteLine("введите число A = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите число B = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int result = Stepen(a,b);
+Console.WriteLine($"-> {result}");
