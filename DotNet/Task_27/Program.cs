a0145121677dab02fd3cf0f78088fd25ac0d7051
Alexey <a.v.alyokhin@mail.ru>
2022-07-05 13:12:51 +0500

    dobavil Task_27 folder - for DZ#27 - rabotaet

---
+++ DotNet/Task_27/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+Console.WriteLine(@"Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+");
+
+int Summator(int in_chislo)
+{
+    int valueForReturn = 0;
+
+    checked
+    {
+        while (in_chislo > 0)
+        {
+            valueForReturn += in_chislo % 10;
+            in_chislo /= 10;
+        }
+    }
+
+
+    return valueForReturn;
+
+}
+
+Console.WriteLine("введите число N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+int result = Summator(n);
+Console.WriteLine($"-> {result}");
