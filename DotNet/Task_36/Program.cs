5b4b6f41ac1806b59d2dfede1a1b6c3e45a9ca24
Alexey <a.v.alyokhin@mail.ru>
2022-07-08 13:31:19 +0500

    dobavil Task_36 folder - DZ#36 - rabotaet

---
+++ DotNet/Task_36/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+
+Console.WriteLine(@"Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+");
+
+int[] RandomArrayCreator(int in_arrSize)
+{
+    var randomGod = new Random();
+    int[] vFrRtrn = new int[in_arrSize];
+
+    for (int i = 0; i < vFrRtrn.Length; i++)
+    {
+        vFrRtrn[i] = randomGod.Next(1, 100);
+    }
+
+    return vFrRtrn;
+
+}
+
+int OddSummator(int[] in_array)
+{
+    int vFrRtrn = 0;
+    for (int i = 0; i < in_array.Length; i++)
+    {
+
+        vFrRtrn = Convert.ToBoolean(i % 2) ? vFrRtrn + in_array[i] : vFrRtrn;
+
+    }
+
+    return vFrRtrn;
+
+}
+
+void PrintResult(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0) Console.Write("Массив = [");
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i] + "]");
+    }
+}
+
+int[] arrayForTask = RandomArrayCreator(8);
+PrintResult(arrayForTask);
+int oddSummator = OddSummator(arrayForTask);
+Console.WriteLine($" Сумма нечётных чисел = {oddSummator}");
