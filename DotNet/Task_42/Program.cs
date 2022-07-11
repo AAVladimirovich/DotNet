ee4b61bfa29efadb55632bff61488c7ea86b8491
Alexey <a.v.alyokhin@mail.ru>
2022-07-11 15:14:24 +0500

    ebral kommentarii - ostavil odin kod v Task_42 zadache

--- DotNet/Task_42/Program.cs
+++ DotNet/Task_42/Program.cs
@@ -6,35 +6,13 @@
 Console.Clear();
 
 Console.WriteLine("Введите число: ");
-
 int a = int.Parse(Console.ReadLine());
-// int k = 0;
-string num = " ";
-
-// int Decimal(int a)
-// {
-//     int k =0;
-//     if (a == 0) return;
-    
-//     if (a > 0) 
-//     {
-//         a = a % 2;
-//         k = a % 2;
-//        Decimal(a);
-//        Console.Write($"{a}");
-//     }
-
-    
-// }
 
 int TenToTwo(int n)
 {
     if (n == 1) return 1;
     if (n == 0) return 0;
-    return n%2+10*TenToTwo(n/2);
+    return n % 2 + 10 * TenToTwo(n / 2);
 }
-
-
-//TenToTwo(a);
 
 Console.WriteLine(TenToTwo(a));
