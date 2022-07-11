7d43dddf247d2d8cf6bed5e19dcaeea97eaff7c3
Alexey <a.v.alyokhin@mail.ru>
2022-07-11 15:03:50 +0500

    dobavil Lection_04, Task_39, Task_40, Task_42 folders, - rabota na seminare

---
+++ DotNet/Lection_04/Program.cs
@@ -0,0 +1,62 @@
+﻿// работа с двумерными массивами
+
+// string[,] table = new string[2,5];
+// table[1,2] = "слово";
+
+// for (int row = 0; row < 2; row++)
+// {
+//     for (int column = 0; column < 5; column++)
+//     {
+//         Console.WriteLine($"-{table[row,column]}-");
+//     }
+
+// }
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+
+        Console.WriteLine();
+
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+// int Factorial (int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial (n-1);
+// }
+
+// Console.WriteLine(Factorial(5));
+
+int Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+for (int i = 1; i < 50; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
---
+++ DotNet/Task_39/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 39: Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int[] RandomArrayCreator(int in_arrSize, int in_minVal, int in_maxVal)
+{
+    var randomGod = new Random();
+    int[] vFrRtrn = new int[in_arrSize];
+
+    for (int i = 0; i < vFrRtrn.Length; i++)
+    {
+        vFrRtrn[i] = randomGod.Next(in_minVal, in_maxVal);
+    }
+
+    return vFrRtrn;
+
+}
+
+void PrintResult(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0) Console.Write(" Массив = [");
+        if (i < arr.Length - 1) Console.Write(arr[i] + ",");
+        else Console.Write(arr[i] + "]");
+    }
+}
+
+int[] Reverse(int[] in_array)
+{
+
+    int index1=0;
+    int index2=in_array.Length-1;
+    int[] temp_array = new int[in_array.Length]; 
+
+    while(index1<in_array.Length)
+    {
+        temp_array[index2] = in_array[index1];
+        index1 ++;
+        index2 --;
+    }
+
+    return temp_array;
+}
+
+int[] arVal = RandomArrayCreator(8, 1, 100);
+PrintResult(arVal);
+arVal = Reverse(arVal);
+//Array.Reverse(arVal);
+PrintResult(arVal);
