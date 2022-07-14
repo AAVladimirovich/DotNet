2d858d45c89b6bd9e9749c0ca4f89ab141cdf30e
Alexey <a.v.alyokhin@mail.ru>
2022-07-14 22:46:56 +0500

    dobavil folder Seminar_7 - metodi dlya raboti s matrix array, print matrix array

---
+++ DotNet/Seminar_7/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.Clear();
+Console.WriteLine(@"// Задача 46: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+");
+
+// int[] date = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int[,] CreateMatrixRndInt(int maxRow, int maxColumn, int min, int max)
+{
+    int[,] arr = new int[maxRow, maxColumn];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+
+    }
+    return arr;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < arr.GetLength(1) - 1) Console.Write(arr[i, j] + ",");
+            else Console.Write(arr[i, j] + "]");
+        }
+        Console.WriteLine();
+
+    }
+}
+
+int[,] arrayResult = CreateMatrixRndInt(3,4,1,23);
+PrintMatrix(arrayResult);
