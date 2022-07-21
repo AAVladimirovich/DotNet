ac79883f9181a92f8ea092d08d3f3860b129f972
Alexey <a.v.alyokhin@mail.ru>
2022-07-21 20:30:11 +0300

    Task_63 - rabota na seminare zapisal

---
+++ DotNet/Task_63/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+void NaturalNumbers(int argNum)
+{
+    if (argNum == 0) return;
+    NaturalNumbers(argNum -1);
+    Console.Write($"{argNum} ");
+}
+
+Console.Clear();
+Console.WriteLine(@"Задача 63: Задайте значение N. Напишите программу, которая выведет
+все натуральные числа в промежутке от 1 до N.
+N = 5 -> 1, 2, 3, 4, 5
+N = 6 -> 1, 2, 3, 4, 5, 6
+");
+
+Console.WriteLine("Задайте число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+NaturalNumbers(num);
