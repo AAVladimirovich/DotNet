386d680e87e6ea800fc8be9fa389453207dc8a15
Alexey <a.v.alyokhin@mail.ru>
2022-07-22 12:29:00 +0300

    sozdal Folder Task_68 - DZ#68 - funkciya Akermana cherez rekursiyu

---
+++ DotNet/Task_68/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+    else if (m > 0 && n > 0) return Akkerman(m - 1, Akkerman(m, n - 1));
+    return 0;
+}
+
+Console.Clear();
+Console.WriteLine(@"Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 3, n = 2 -> A(m,n) = 29
+");
+
+Console.WriteLine("Задайте число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Функция Аккермана = {Akkerman(m, n)}");
