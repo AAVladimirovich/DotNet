20278847b05a5a4f1e44e3f2be4eebc824dbc151
Alexey <a.v.alyokhin@mail.ru>
2022-07-22 11:22:06 +0300

    dobavleni folders Task_69/Task_67 zadachami razbor na seminare

---
+++ DotNet/Task_67/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+void SummNumbers(int argNum, int sum)
+{
+    int remain = argNum % 10;
+    sum += remain;
+    if (remain == 0) 
+    {
+        Console.Write($"{sum} ");
+        return;
+    }
+    SummNumbers(argNum / 10, sum );
+}
+
+Console.Clear();
+Console.WriteLine(@"Задача 67: Напишите программу, которая будет принимать на вход число и
+возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+");
+
+Console.WriteLine("Задайте число ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SummNumbers(n,0);
